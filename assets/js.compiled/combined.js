128234885c6997c008a06b3bceaf5a94088288fa
Rona Kilmer <Rona@crowdfavorite.com>
2015-08-18 08:16:33 -0600

    switched logic to loop through array for efficiency

--- assets/js.compiled/combined.js
+++ assets/js.compiled/combined.js
@@ -32401,10 +32401,10 @@ console.log('Non-Angular App JS');
 			return directive;
 
 
-
 			function linkFunction($scope, $elem, $attrs) {
 				// set all the initial values to 0
-				var coins = {
+				var coinsArr = [200, 100, 50, 2, 1], // value of the coins highest to lowest
+					coinResults = { // where we will store the coin counts
 						pence200: 0,
 						pence100: 0,
 						pence50: 0,
@@ -32415,7 +32415,7 @@ console.log('Non-Angular App JS');
 					errorText ='';
 
 				// make the values accessible to the template
-				$scope.coins = coins;
+				$scope.coinResults = coinResults;
 				$scope.totalPence = totalPence;
 				$scope.errorText = errorText;
 
@@ -32426,22 +32426,21 @@ console.log('Non-Angular App JS');
 						roundedNum,
 						pattern = /[^0-9.-]+/g; // get rid of the symbols
 
-					// check to make sure the value doesn't include any disallowed characters
 					// @TODO £ can only start the string value.match(/^£/)
 					// @TODO p can only end the string value.match(/p$/)
-					// if the value starts with the £ or has a decimal then multiply by 100
-					// remove leading zeros
+					// @TODO remove leading zeros
 
+					// check to make sure the value doesn't include any disallowed characters
 					if (value.match(/[^£p.\d]/)) {
 						$scope.errorText = 'That is not a valid entry.';
 					}
 					else {
 						// start by clearing any old values
-						$scope.coins.pence200 = 0;
-						$scope.coins.pence100 = 0;
-						$scope.coins.pence50 = 0;
-						$scope.coins.pence2 = 0;
-						$scope.coins.pence1 = 0;
+						$scope.coinResults.pence200 = 0;
+						$scope.coinResults.pence100 = 0;
+						$scope.coinResults.pence50 = 0;
+						$scope.coinResults.pence2 = 0;
+						$scope.coinResults.pence1 = 0;
 
 						newNum = value.replace(pattern, '');
 						roundedNum = Math.round(newNum * 100) / 100;
@@ -32451,7 +32450,6 @@ console.log('Non-Angular App JS');
 							// using parseInt to fix floating point issues (http://floating-point-gui.de/)
 							$scope.totalPence = parseInt(roundedNum * 100);
 							numCoins($scope.totalPence);
-
 						}
 						else {
 							$scope.totalPence = roundedNum;
@@ -32463,47 +32461,14 @@ console.log('Non-Angular App JS');
 				}
 
 				function numCoins(pence) {
-					// This works but it's a lot of code.
-					// Let's find a better way.
+					for (var i = 0; i < coinsArr.length; i++) {
+						var coinCount = Math.floor(pence / coinsArr[i]), // don't worry about the remainder
+							coinName = 'pence' + coinsArr[i]; // create the result name
 
-					if (pence >= 200) {
-						get200(pence);
-					}
-					else if (pence >= 100) {
-						get100(pence);
-					}
-					else if (pence >= 50) {
-						get50(pence);
-					}
-					else if (pence >= 2) {
-						get2(pence);
-					}
-					else {
-						get1(pence);
-					}
+						$scope.coinResults[coinName] = coinCount;
 
-					function get200(pence) {
-						var remainder = pence % 200;
-						$scope.coins.pence200 = Math.floor(pence/200);
-						get100(remainder);
-					}
-					function get100(pence) {
-						var remainder = pence % 100;
-						$scope.coins.pence100 = Math.floor(pence/100);
-						get50(remainder);
-					}
-					function get50(pence) {
-						var remainder = pence % 50;
-						$scope.coins.pence50 = Math.floor(pence/50);
-						get2(remainder);
-					}
-					function get2(pence) {
-						var remainder = pence % 2;
-						$scope.coins.pence2 = Math.floor(pence/2);
-						get1(remainder);
-					}
-					function get1(pence) {
-						$scope.coins.pence1 = pence;
+						// reduce the current coin's amount from the total pence before proceeding
+						pence -= coinCount * coinsArr[i];
 					}
 				}
 			}
